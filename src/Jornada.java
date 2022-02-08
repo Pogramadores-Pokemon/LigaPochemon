b0430a3403fe085fad6ddd278840cfa6216c5d4b
AlejandroPascualMoreno <93983959+AlejandroPascualMoreno@users.noreply.github.com>
2022-02-08 09:16:09 +0100

    Partido y Jornada

--- src/Jornada.java
+++ src/Jornada.java
@@ -1,4 +1,26 @@
 
 public class Jornada {
-
+	private Arbitro [] arbitro;
+	private Partido [] partido;
+	private int horario;
+	
+	public Arbitro[] getArbitro() {
+		return arbitro;
+	}
+	public void setArbitro(Arbitro[] arbitro) {
+		this.arbitro = arbitro;
+	}
+	public Partido[] getPartido() {
+		return partido;
+	}
+	public void setPartido(Partido[] partido) {
+		this.partido = partido;
+	}
+	public int getHorario() {
+		return horario;
+	}
+	public void setHorario(int horario) {
+		this.horario = horario;
+	}
+	
 }
---
+++ src/Partido.java
@@ -0,0 +1,39 @@
+
+public class Partido {
+	private Arbitro [] arbitro;
+	private int golfavor;
+	private int golcontra;
+	private Equipo equipolocal;
+	private Equipo equipovisit;
+	
+	public Arbitro[] getArbitro() {
+		return arbitro;
+	}
+	public void setArbitro(Arbitro[] arbitro) {
+		this.arbitro = arbitro;
+	}
+	public int getGolfavor() {
+		return golfavor;
+	}
+	public void setGolfavor(int golfavor) {
+		this.golfavor = golfavor;
+	}
+	public int getGolcontra() {
+		return golcontra;
+	}
+	public void setGolcontra(int golcontra) {
+		this.golcontra = golcontra;
+	}
+	public Equipo getEquipolocal() {
+		return equipolocal;
+	}
+	public void setEquipolocal(Equipo equipolocal) {
+		this.equipolocal = equipolocal;
+	}
+	public Equipo getEquipovisit() {
+		return equipovisit;
+	}
+	public void setEquipovisit(Equipo equipovisit) {
+		this.equipovisit = equipovisit;
+	}
+}
